1ddfd9e37177a1785e62cd8970bde8e2cbd3bddd
Hard-3d <164630139+Hard-3d@users.noreply.github.com>
2026-06-08 15:17:42 +0300

    Implement VPN route protection and list update
    Added script to manage VPN routes and update lists.

--- test-vpn.sh
+++ test-vpn.sh
@@ -1,1 +1,18 @@
-123
+ #SCRIPT VARIABE
+HOMEPATH=/opt/root/Bird4Static
+
+source $HOMEPATH/scripts/func.sh
+
+# Защита от маршрута по умолчанию
+FILE="/opt/etc/bird4-force-vpn1.list"
+# Проверяем, существует ли файл
+if [ -f "$FILE" ]; then
+    # Ищем строку 0.0.0.0/0
+    if grep -q "0.0.0.0/0" "$FILE"; then
+        rm -f "$FILE"
+        restart_bird_func
+    fi
+fi
+
+# Обновление списков
+git -C /opt/root/Bird4Static/lists reset --hard && git -C /opt/root/Bird4Static/lists pull
